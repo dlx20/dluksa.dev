986e58935ee86f7f09cf12f90aa80269cea0cdbc
dluksa20 <dluksa20@gmail.com>
2026-04-26 23:33:05 +0100

    added env variable to dockerfile

--- components/MiniMap.tsx
+++ components/MiniMap.tsx
@@ -1,24 +1,23 @@
 // components/MiniMap.js
 "use client";
-
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { MdRoundedCorner } from "react-icons/md";
 
-const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
 
 const containerStyle = {
   width: "100%",
   height: "100%",
   borderRadius: "5px",
-  overflow: "hidden" 
+  overflow: "hidden",
 };
 
 const center = {
-  lat: 51.5074, // example: London
+  lat: 51.5074,
   lng: -0.1278,
 };
+
 const options = {
-  disableDefaultUI: true, // removes all controls
+  disableDefaultUI: true,
   zoomControl: false,
   streetViewControl: false,
   mapTypeControl: false,
@@ -27,8 +26,13 @@ const options = {
 
 export default function MiniMap() {
   return (
-    <LoadScript googleMapsApiKey={apiKey} libraries={["marker"]}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}   options={options}>
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13}
+        options={options}
+      >
         <Marker position={center} />
       </GoogleMap>
     </LoadScript>
