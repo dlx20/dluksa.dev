73bc31b86a79176255b2b1acb8e57f96ff10c4d3
dluksa20 <dluksa20@gmail.com>
2026-04-27 00:16:19 +0100

    added env variable to dockerfile

--- components/MiniMap.tsx
+++ components/MiniMap.tsx
@@ -1,40 +1,27 @@
 // components/MiniMap.js
 "use client";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React from 'react';
+import {createRoot} from "react-dom/client";
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
 
 const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
-
-const containerStyle = {
-  width: "100%",
-  height: "100%",
-  borderRadius: "5px",
-  overflow: "hidden",
-};
 
 const center = {
   lat: 51.5074,
   lng: -0.1278,
 };
 
-const options = {
-  disableDefaultUI: true,
-  zoomControl: false,
-  streetViewControl: false,
-  mapTypeControl: false,
-  fullscreenControl: false,
-};
 
 export default function MiniMap() {
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={13}
-        options={options}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
+      <Map
+        defaultZoom={13}
+        defaultCenter={{ lat: 51.5074, lng: -0.1278}}
+        onCameraChanged={(ev: MapCameraChangedEvent) =>
+          console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+        }>
+      </Map>
+    </APIProvider>
   );
 }
