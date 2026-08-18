3be1d24ef6ef65d93bf4f36f2a4796ed9726a5a3
ddev-dotdev <ddev@dluksa.dev>
2026-08-18 14:49:30 +0100

    Systems setings section, title added for theme selection

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,12 +1,29 @@
 import { THEMES } from "@/lib/constants"
 import Button from "./ui/Button"
 import { useTheme } from "next-themes"
+import { MdOutlinePalette } from 'react-icons/md'
 
 
 const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme()
     return (
-        <div className="flex flex-col justify-center gap-4">
+        <div className="flex flex-col justify-center">
+
+            <div className="mb-5 flex items-center gap-4">
+                <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent/10">
+                    <MdOutlinePalette
+                        size={20}
+                        className="fill-accent"
+                    />
+                </div>
+
+                <div>
+                    <h2 className="font-semibold text-accent">
+                        Appearance
+                    </h2>
+                </div>
+            </div>
+            
             <div className="grid grid-cols-2 gap-2 *:last:col-span-2">
                 {THEMES.map(({ id, label }) => (
                     <Button
